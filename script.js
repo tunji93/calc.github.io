a9413cae1539f011c202b7d8985ac62d42f21fcf
wahab <aderintoadetunji@gmail.com>
2021-04-10 15:11:49 +0100

    final calculator

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const numbers = document.querySelectorAll('.number')
-const operators = document.querySelectorAll(".operators")
+const operators = document.querySelectorAll(".operator")
 const equals = document.querySelector('.equals')
 const clear = document.querySelector('.clear')
 const percentage = document.querySelector('.percentage')
+const del = document.querySelector('.del')
 
 const operations = document.querySelector('#operation')
 const result = document.querySelector('#result')
 
-console.log(numbers)
+console.log(operators)
 
 class Calculator  {
     constructor(operations, result) {
@@ -19,7 +20,13 @@ class Calculator  {
     }
 
     display() {
-        this.operations.innerText = this.activeOperand
+        if(this.activeOperand.toString().length> 19) {
+            alert(" The value you entered is greater than the allowable limit, kindly restart calculator" )
+            return 
+        }
+
+        this.result.innerText = this.activeOperand
+        this.operations.innerText = this.previousOperand
 
 
     }
@@ -32,7 +39,15 @@ class Calculator  {
         
 
     }
-    chooseOperation() {
+    chooseOperation(operator) {
+        if(this.activeOperand === " ") return
+        if(this.previousOperand !== " ") {
+            this.compute()
+        }
+        this.operator = operator
+        this.previousOperand = this.activeOperand
+        this.activeOperand= " "
+
 
     }
     init() {
@@ -41,7 +56,34 @@ class Calculator  {
         this.operator= null
     }
     compute() {
+      let computation 
+      let prev = parseFloat(this.previousOperand)  
+      let curr = parseFloat(this.activeOperand)
+      if (isNaN(prev) || isNaN(curr)) return
+      if(this.operator === "+") {
+        computation = prev + curr
+      }
+      else if(this.operator === "-") {
+        computation = prev - curr
+      }
+      else if(this.operator === "*") {
+        computation = prev * curr
+      }
+      else if(this.operator === "÷") {
+        computation = prev / curr
+      }
+      else {return}
+      this.activeOperand = computation
+      this.operation = null
+      this.previousOperand = ""
 
+    }
+    delete() {
+        if(this.activeOperand.toString().length === 0) return
+        this.activeOperand = this.activeOperand.toString().slice(0,-1)
+    }
+    computePercentage() {
+        this.activeOperand = this.activeOperand / 100
     }
 }
 
@@ -54,4 +96,33 @@ numbers.forEach(button => {
        machine.display()
     })
 
+})
+
+operators.forEach(button => {
+    button.addEventListener('click' ,() => {
+       let operator = button.innerText
+       machine.chooseOperation(operator)
+       machine.display()
+    })
+
+})
+
+equals.addEventListener('click', ()=> {
+    machine.compute()
+    machine.display()
+})
+clear.addEventListener('click', ()=> {
+    machine.init()
+    machine.display()
+   
+})
+del.addEventListener('click', ()=> {
+    machine.delete()
+    machine.display()
+   
+})
+percentage.addEventListener('click', ()=> {
+    machine.computePercentage()
+    machine.display()
+   
 })
